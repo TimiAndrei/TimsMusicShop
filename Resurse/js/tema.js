adb39cb8f52ba55ac5875a84e500bc8e99856a12
TimiAndrei <timiandrei223@gmail.com>
2023-05-27 17:26:51 +0300

    updated theme switch and fixed footer bug

--- Resurse/js/tema.js
+++ Resurse/js/tema.js
@@ -4,22 +4,13 @@ if (tema)
     document.body.classList.add("dark");
 
 window.addEventListener("DOMContentLoaded", function () {
+    if (document.body.classList.contains("dark")) {
+        document.getElementById("tema").checked = "true";
+    }
     document.getElementById("tema").onclick = function () {
-        //     if (document.body.classList.contains("dark")) {
-        //         document.body.classList.remove("dark");
-        //         localStorage.removeItem("tema");
-        //     }
-        //     else {
-        //         document.getElementById("tema").checked = "true"; // nu merge 
-        //         document.body.classList.add("dark");
-        //         localStorage.setItem("tema", "dark");
-        //     }
-        // }
-        if (!document.getElementById("tema").checked) {
-            if (document.body.classList.contains("dark")) {
-                document.body.classList.remove("dark");
-                localStorage.removeItem("tema");
-            }
+        if (document.body.classList.contains("dark")) {
+            document.body.classList.remove("dark");
+            localStorage.removeItem("tema");
         }
         else {
             document.body.classList.add("dark");
