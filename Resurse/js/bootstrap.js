196ed20a9c09365925252e456c3d7af953e0dbdd
TimiAndrei <timiandrei223@gmail.com>
2023-05-27 17:59:34 +0300

    modified function with setTimeout

--- Resurse/js/bootstrap.js
+++ Resurse/js/bootstrap.js
@@ -1,26 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
     const cards = document.getElementsByClassName('produs');
 
-    // Function to display a card with a delay
-    function displayCardWithDelay(card, delay) {
-        setTimeout(function () {
-            card.style.display = 'block'; // Show the card
-        }, delay);
-    }
-
     // Delay between each card (in milliseconds)
-    const delayBetweenCards = 500;
+    const delayBetweenCards = 250;
 
-    // Hide all the cards initially
-    for (let i = 0; i < cards.length; i++) {
-        const card = cards[i];
-        card.style.display = 'none';
+    function fadeInCard(card, index) {
+        setTimeout(function () {
+            card.classList.add('card-appear');
+        }, delayBetweenCards * index);
     }
 
-    // Loop through each card and display with a delay
+    // Loop through each card and set animation delay
     for (let i = 0; i < cards.length; i++) {
         const card = cards[i];
-        const delay = (i + 1) * delayBetweenCards; // Increase delay for each card
-        displayCardWithDelay(card, delay);
+        fadeInCard(card, i);
     }
 });
+
+
+// On mouse scroll version
+// document.addEventListener('DOMContentLoaded', function () {
+//     const cards = document.getElementsByClassName('produs');
+//     const delayBetweenCards = 500;
+
+    // function fadeInCard(card, index) {
+    //     setTimeout(function () {
+    //         card.classList.add('card-appear');
+    //     }, delayBetweenCards * index);
+    // }
+
+//     function isInViewport(element) {
+//         const rect = element.getBoundingClientRect();
+//         return (
+//             rect.top >= 0 &&
+//             rect.left >= 0 &&
+//             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//         );
+//     }
+
+//     function handleScroll() {
+//         for (let i = 0; i < cards.length; i++) {
+//             const card = cards[i];
+//             if (isInViewport(card)) {
+//                 card.classList.add('card-appear');
+//             }
+//         }
+//     }
+
+//     window.addEventListener('scroll', handleScroll);
+// });
